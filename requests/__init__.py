7dee56c8c9ca8b0b6b3946fc9815242a0c3fdcfb
QuantumKeyUYU <uyu.enterglobal@gmail.com>
2025-07-16 19:15:24 +0300

    Fix requests stub and semgrep workflow

--- requests/__init__.py
+++ requests/__init__.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
 
+class exceptions:
+    class SSLError(Exception):
+        pass
+
+
 def post(url: str, *args, **kwargs):
     class Resp:
         status_code = 200
